936bc498804c2e212abf09dcafea89c1871dc426
alan <2104636396@qq.com>
2026-04-29 01:22:22 +0800

    fix: 修复RabbitMQ消息反序列化错误，改用JSON序列化

---
+++ mall/mall-common/src/main/java/com/macro/mall/common/domain/EsProductMessage.java
@@ -0,0 +1,32 @@
+package com.macro.mall.common.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 商品同步消息
+ * Created by macro on 2026/4/27.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EsProductMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 商品ID
+     */
+    private Long productId;
+    
+    /**
+     * 操作类型：ADD-新增，UPDATE-修改，DELETE-删除
+     */
+    private String actionType;
+    
+    /**
+     * 时间戳
+     */
+    private Long timestamp;
+}
