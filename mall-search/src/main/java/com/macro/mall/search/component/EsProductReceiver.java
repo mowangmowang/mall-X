43e034d2108a992dc22d728d1fc040101d622b1b
alan <2104636396@qq.com>
2026-04-29 10:16:41 +0800

    fix: 修复商品创建时ID为null导致ES同步失败等3个问题
    1. PmsProductController: create()不再依赖productParam.getId()，
       改为由service返回生成的productId，确保MQ消息正常发送
    2. EsProductServiceImpl: importAll()添加分页，防止大数据量OOM
    3. EsProductReceiver: 移除try-catch吞异常，让Spring AMQP自动重试

---
+++ mall-search/src/main/java/com/macro/mall/search/component/EsProductReceiver.java
@@ -0,0 +1,37 @@
+package com.macro.mall.search.component;
+
+import com.macro.mall.common.domain.EsProductMessage;
+import com.macro.mall.search.service.EsProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 商品同步消息接收器
+ * Created by macro on 2026/4/27.
+ */
+@Component
+@RabbitListener(queues = "mall.product.update")
+public class EsProductReceiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EsProductReceiver.class);
+    
+    @Autowired
+    private EsProductService esProductService;
+    
+    @RabbitHandler
+    public void handle(EsProductMessage message) {
+        LOGGER.info("接收到商品同步消息：productId={}, actionType={}",
+            message.getProductId(), message.getActionType());
+
+        if ("ADD".equals(message.getActionType()) || "UPDATE".equals(message.getActionType())) {
+            esProductService.create(message.getProductId());
+            LOGGER.info("商品索引更新成功：productId={}", message.getProductId());
+        } else if ("DELETE".equals(message.getActionType())) {
+            esProductService.delete(message.getProductId());
+            LOGGER.info("商品索引删除成功：productId={}", message.getProductId());
+        }
+    }
+}
