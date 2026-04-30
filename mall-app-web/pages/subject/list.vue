6d8baa2961b91fec57f69c8ce79de0c73d3f2cf4
alan <2104636396@qq.com>
2026-04-30 20:15:16 +0800

    fix: 专题列表图片为空时显示CSS渐变动画占位图

--- mall-app-web/pages/subject/list.vue
+++ mall-app-web/pages/subject/list.vue
@@ -3,7 +3,11 @@
 		<view class="subject-list">
 			<view v-for="(item, index) in subjectList" :key="index" class="subject-item" @click="navToDetail(item)">
 				<view class="image-wrapper">
-					<image :src="item.pic" mode="aspectFill"></image>
+					<image :src="item.pic" mode="aspectFill" v-if="item.pic"></image>
+					<view class="img-placeholder" v-else>
+						<text class="placeholder-icon">✦</text>
+						<text class="placeholder-text">{{item.title ? item.title.substring(0, 4) : '专题'}}</text>
+					</view>
 				</view>
 				<view class="info">
 					<text class="title clamp">{{item.title}}</text>
@@ -83,6 +87,38 @@
 			flex-shrink: 0;
 			margin-right: 20upx;
 			image { width: 100%; height: 100%; }
+		}
+
+		.img-placeholder {
+			width: 100%;
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			background: linear-gradient(135deg, #a8edea 0%, #fed6e3 50%, #d4fc79 100%);
+			background-size: 200% 200%;
+			animation: gradientShift 4s ease infinite;
+
+			.placeholder-icon {
+				font-size: 48upx;
+				color: rgba(255,255,255,0.9);
+				text-shadow: 0 2upx 8upx rgba(0,0,0,0.1);
+			}
+
+			.placeholder-text {
+				font-size: 22upx;
+				color: rgba(255,255,255,0.85);
+				margin-top: 8upx;
+				font-weight: 500;
+				text-shadow: 0 1upx 4upx rgba(0,0,0,0.08);
+			}
+		}
+
+		@keyframes gradientShift {
+			0% { background-position: 0% 50%; }
+			50% { background-position: 100% 50%; }
+			100% { background-position: 0% 50%; }
 		}
 
 		.info {
